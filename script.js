d7b26000fc5ece2ba4be7f38376b665def6bac79
Kevin <apprentice@master-inf-4.local>
2014-07-03 08:48:38 -0700

    firebase complete ready for deploy

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-// msg you want to send to firebase
-// var myDataRef = new Firebase('https://w9pvpyjxjul.firebaseio-demo.com/');
-//       $('#messageInput').keypress(function (e) {
-//         if (e.keyCode == 13) {
-//           var name = $('#nameInput').val();
-//           var text = $('#messageInput').val();
-//           myDataRef.push({name: name, text: text});
-//           $('#messageInput').val('');
-//         }
-//       });
-//       myDataRef.on('child_added', function(snapshot) {
-//         var message = snapshot.val();
-// displayChatMessage(message.name, message.text);
-//       });
+
 $(function(){
   var player = "<div id='cutie'></div>";
   $('#cutieWorld').append(player);
 
   $(document).keydown(function(e){
     var myDataRef = new Firebase('https://intense-fire-1019.firebaseIO-demo.com/positions/');
-    // alert(e.keyCode);
-    // var myUser = usersRef.child(escapeEmailAddress('hello@hello.com'))
-// myUser.set({ email: 'hello@hello.com', name: 'Alex', phone: 12912912 });
     var positionAxis = $('#cutie').position();
     myDataRef.set(
       positionAxis
@@ -40,18 +24,18 @@ $(function(){
         break;
       ;
     }
-    var myDataRefLeft = new Firebase('https://intense-fire-1019.firebaseIO-demo.com/positions/left');
-    var myDataRefTop = new Firebase('https://intense-fire-1019.firebaseIO-demo.com/positions/top');
+  })
+    var myDataRefLeft = new Firebase('https://intense-fire-1019.firebaseIO-demo.com/positions/left/');
+    var myDataRefTop = new Firebase('https://intense-fire-1019.firebaseIO-demo.com/positions/top/');
 
     myDataRefLeft.on('value', function(snapshot) {
-  // alert(snapshot.val());  // Alerts 'San Francisco'
-      console.log(snapshot.val());
+      var leftCoord = snapshot.val();
+      $("#cutie").css('left', leftCoord)
     });
     myDataRefTop.on('value', function(snapshot) {
-  // alert(snapshot.val());  // Alerts 'San Francisco'
-      console.log(snapshot.val());
+      var topCoord = snapshot.val();
+      $("#cutie").css('top', topCoord)
     });
-  })
 });
 
 
